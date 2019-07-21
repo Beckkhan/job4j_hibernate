9a9758b0e8ca80e9a7a562b3a5a32d3bde8b1ec9
Beckkhan <beckkhan@mail.ru>
2019-07-21 17:55:35 +0300

    2. Lambda and wrapper pattern. [#49295]

--- src/main/java/ru/job4j/todo/ToDoListService.java
+++ src/main/java/ru/job4j/todo/ToDoListService.java
@@ -2,43 +2,56 @@ package ru.job4j.todo;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import ru.job4j.todo.models.Item;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Khan Vyacheslav (mailto: beckkhan@mail.ru)
- * @version 1.0
- * @since 18.07.2019
+ * @version 2.0
+ * @since 21.07.2019
  */
 public class ToDoListService {
 
     private SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = factory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T result = command.apply(session);
+            tx.commit();
+            return result;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     public void saveItem(Item item) {
-        Session session = factory.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> session.save(item)
+        );
     }
 
     public List<Item> findAll() {
-        Session session = factory.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from Item", Item.class).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item", Item.class).list()
+        );
     }
 
     public void resetStatus(int id, boolean done) {
-        Session session = factory.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        item.setDone(done);
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> {
+                    Item item = session.get(Item.class, id);
+                    item.setDone(done);
+                    session.update(item);
+                    return true;
+                }
+        );
     }
 }
